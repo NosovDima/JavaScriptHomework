4cae7b970b0fef34b2930e7be4f6c2bd245e52b6
NosovDima <130983082+NosovDima@users.noreply.github.com>
2023-06-07 21:41:25 +0300

    Add files via upload

---
+++ My 2nd Repository/test.js
@@ -0,0 +1,31 @@
+// Вынесли логику связанную с обработчиком и сам обработчик в отдельную функцию
+const initEventListeners = () => {
+    const studentElements = document.querySelectorAll(".student");
+
+    for (const studentElement of studentElements) {
+      studentElement.addEventListener("click", () => {
+        console.log(1);
+      });
+    }
+  };
+
+// При нажатии на кнопку происходит перерисовка контента
+buttonElement.addEventListener("click", () => {
+    ...
+
+          // На этой строке разметка перезаписывается и обработчики слетают
+    listElement.innerHTML =
+      listElement.innerHTML +
+      `<li class="student">
+          <p class="student-name">
+            ${nameInputElement.value}, любимый цвет
+            <span style="color: ${colorInputElement.value}"> ${colorInputElement.value}</span>
+          </p>
+        </li>`;
+
+          // Вызвали функцию, которая повторно нависит обработчики
+          // после перерисовки контента
+    initEventListeners();
+
+    nameInputElement.value = "";
+  });
